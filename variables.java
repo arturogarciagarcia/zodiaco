f27a3b4647544274800a46797a892845831366d4
Arturo Garcia <r2d293@live.com.mx>
2016-09-15 16:55:57 -0700

    se calcula el zodiaco

--- variables.java
+++ variables.java
@@ -24,3 +24,79 @@ public class zodiaco {
 		System.out.print("Mes = ");
 		Scanner mes = new Scanner(System.in);
 		b = mes.nextInt();
+
+		if (a >= 22 && b == 12) {
+			System.out.println("Su signo es capricornio" + "\n tu edad es: " + edad + "\n tu nombre es: " + nombre
+					+ "\n tu No. control: " + "\n dia: " + a + "\n mes: " + b);
+		}
+		if (a < 20 && b == 1) {
+			System.out.println("Su signo es capricornio");
+		}
+		if (a >= 21 && b == 1) {
+			System.out.println("Su signo es acuario");
+		}
+		if (a <= 20 && b == 2) {
+			System.out.println("Su signo es acuario");
+		}
+		if (a >= 21 && b == 2) {
+			System.out.println("Su signo es piscis");
+		}
+		if (a <= 20 && b == 3) {
+			System.out.println("Su signo es piscis");
+		}
+		if (a >= 21 && b == 3) {
+			System.out.println("Su signo es aries");
+		}
+		if (a <= 19 && b == 4) {
+			System.out.println("Su signo es aries");
+		}
+		if (a >= 20 && b == 4) {
+			System.out.println("Su signo es tauro");
+		}
+		if (a <= 20 && b == 5) {
+			System.out.println("Su signo es tauro");
+		}
+		if (a >= 21 && b == 5) {
+			System.out.println("Su signo es geminis");
+		}
+		if (a <= 21 && b == 6) {
+			System.out.println("Su signo es geminis");
+		}
+		if (a >= 22 && b == 6) {
+			System.out.println("Su signo es cancer");
+		}
+		if (a <= 21 && b == 7) {
+			System.out.println("Su signo es cancer");
+		}
+		if (a >= 22 && b == 7) {
+			System.out.println("Su signo es leo");
+		}
+		if (a <= 21 && b == 8) {
+			System.out.println("Su signo es leo");
+		}
+		if (a >= 22 && b == 8) {
+			System.out.println("Su signo es Virgo");
+		}
+		if (a <= 22 && b == 9) {
+			System.out.println("Su signo es Virgo");
+		}
+		if (a >= 23 && b == 9) {
+			System.out.println("Su signo es Libra");
+		}
+		if (a <= 21 && b == 10) {
+			System.out.println("Su signo es Libra");
+		}
+		if (a >= 22 && b == 10) {
+			System.out.println("Su signo es Escorpio" + "\n tu edad es: " + edad + "\n tu nombre es: " + nombre
+					+ "\n tu No. control: " + "/n dia: " + a + "/n mes: " + b);
+		}
+		if (a <= 21 && b == 11) {
+			System.out.println("Su signo es Escorpio" + "\n tu edad es: " + edad + "\n tu nombre es: " + nombre
+					+ "\n tu No. control: " + "/n dia: " + a + "/n mes: " + b);
+		}
+		if (a >= 22 && b == 11) {
+			System.out.println("Su signo es Sagitario");
+		}
+		if (a <= 21 && b == 12) {
+			System.out.println("Su signo es Sagitario");
+		}
