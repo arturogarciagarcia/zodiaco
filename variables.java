128cabb160e7a36e63ce947b41e7fc9f5547e923
Arturo Garcia <r2d293@live.com.mx>
2016-09-15 16:53:19 -0700

    se piden los valores para las variables

--- variables.java
+++ variables.java
@@ -6,3 +6,21 @@ public class zodiaco {
 	public static void main(String args[]) {
 		int a, b, edad, nocontrol;
 		String nombre, apellido;
+
+		System.out.println("Dame tu nombre: ");
+		Scanner name = new Scanner(System.in);
+		nombre = name.nextLine();
+		System.out.println("Cual es tu edad: ");
+		Scanner years = new Scanner(System.in);
+		edad = years.nextInt();
+		System.out.println("Cual es el numero de control: ");
+		Scanner control = new Scanner(System.in);
+		nocontrol = control.nextInt();
+
+		System.out.println("Ingrese el día y el mes de su nacimiento");
+		System.out.print("Dia = ");
+		Scanner dia = new Scanner(System.in);
+		a = dia.nextInt();
+		System.out.print("Mes = ");
+		Scanner mes = new Scanner(System.in);
+		b = mes.nextInt();
